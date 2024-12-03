c29fc4d977828adc4c2173cb0814eb24a1c7661b
Donik03050208 <goncharov-bogdan@internet.ru>
2024-12-03 18:29:26 +0300

    Added equals and hashcode

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,21 +1,33 @@
 package model;
 
 
-    public class Student {
-        private String name;
+import java.util.Objects;
 
-        public Student(String name) {
-            this.name = name;
-        }
+public class Student {
+    private String name;
 
-        public String getName() {
-            return name;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
+    public Student(String name) {
+        this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+
+    }
+}
 
 
